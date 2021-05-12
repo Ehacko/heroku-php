e824bcd3ba95d85dab36543db568555cabeb9da3
Ehacko <Darkdav.T@gmail.com>
2021-05-12 06:53:15 -0400

    a

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['php']->render('codeigniter/index.php');
+  return 'hello world';
 });
 
 $app->run();
